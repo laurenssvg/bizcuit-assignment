961c91cf6f0e16012dc315d562347ab55a2f28a7
laurenssvg <l.vangellecum@gmail.com>
2020-01-29 23:08:47 +0100

    removed readme.md

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      statements: []
+    };
+  }
+
+  componentDidMount() {
+    let data = require("./statements.json");
+    this.setState({ statements: data });
+  }
+
+  render() {
+    const { statements } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Bank account:</h1>
+        {statements.map(statement => (
+          <ul>
+            <li>{statement.iban}</li>
+          </ul>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
